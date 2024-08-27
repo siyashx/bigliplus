21e2fed6935cb8b6af08a343f98e065a9dbe04e4
siyashx <siyashxyusifov@gmail.com>
2024-08-27 20:26:24 +0400

    .

--- src/main/java/com/biglikuryer/bigliplus/api/chat/controller/ChatController.java
+++ src/main/java/com/biglikuryer/bigliplus/api/chat/controller/ChatController.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @Controller
-@RequestMapping("/api/v3")
+@RequestMapping("/api")
 public class ChatController {
 
     @Autowired
