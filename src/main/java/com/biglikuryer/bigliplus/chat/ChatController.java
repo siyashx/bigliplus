7feef41faca64c3b12c71543d314f20b2f817152
siyashx <siyashxyusifov@gmail.com>
2024-08-27 13:22:36 +0400

    .

---
+++ src/main/java/com/biglikuryer/bigliplus/chat/ChatController.java
@@ -0,0 +1,18 @@
+package com.biglikuryer.bigliplus.chat;
+
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class ChatController {
+
+    @MessageMapping("/chat.sendMessage")
+    @SendTo("/topic/public")
+    public ChatMessage sendMessage(
+            @Payload ChatMessage chatMessage
+    ) {
+        return chatMessage;
+    }
+}
