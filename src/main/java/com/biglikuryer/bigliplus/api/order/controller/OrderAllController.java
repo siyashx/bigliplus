b60cfd6ef6c41c2391d4d1d3e9bafe7f45c70561
siyashx <siyashxyusifov@gmail.com>
2024-09-09 02:22:42 +0400

    .

--- src/main/java/com/biglikuryer/bigliplus/api/order/controller/OrderAllController.java
+++ src/main/java/com/biglikuryer/bigliplus/api/order/controller/OrderAllController.java
@@ -3,25 +3,27 @@ package com.biglikuryer.bigliplus.api.order.controller;
 import com.biglikuryer.bigliplus.dto.order.OrderDto;
 import com.biglikuryer.bigliplus.service.impl.order.OrderServiceImpl;
 import org.springframework.http.ResponseEntity;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@RestController
-@RequestMapping("/api/v3/orders")
+@Controller
 public class OrderAllController {
     private final OrderServiceImpl orderServiceImpl;
+
 
     public OrderAllController(OrderServiceImpl orderServiceImpl) {
         this.orderServiceImpl = orderServiceImpl;
     }
 
-    // List
-    @GetMapping
-    public ResponseEntity<List<OrderDto>> getAllOrders() {
-        List<OrderDto> orderDtoList = orderServiceImpl.getAllOrders();
-        return ResponseEntity.ok(orderDtoList);
+    @MessageMapping("/getOrders")
+    @SendTo("/topic/orders")
+    public List<OrderDto> getAllOrders() {
+        return orderServiceImpl.getAllOrders();
     }
 }
--- src/main/java/com/biglikuryer/bigliplus/config/WebSocketConfig.java
+++ src/main/java/com/biglikuryer/bigliplus/config/WebSocketConfig.java
@@ -12,12 +12,12 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void configureMessageBroker(MessageBrokerRegistry config) {
-        config.enableSimpleBroker("/topic");
+        config.enableSimpleBroker("/topic", "/queue");  // Hem topic hem queue'yu destekle
         config.setApplicationDestinationPrefixes("/app");
     }
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/ws").setAllowedOrigins("*");
+        registry.addEndpoint("/ws").setAllowedOrigins("*").withSockJS();
     }
 }
