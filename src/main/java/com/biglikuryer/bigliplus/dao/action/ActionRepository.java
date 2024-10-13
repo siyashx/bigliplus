5d9a5f58352c74e0a32ac02b169eaf7b6956f7a8
siyashx <siyashxyusifov@gmail.com>
2024-10-13 12:50:00 +0400

    .

--- src/main/java/com/biglikuryer/bigliplus/dao/action/ActionRepository.java
+++
@@ -1,7 +0,0 @@
-package com.biglikuryer.bigliplus.dao.action;
-
-import com.biglikuryer.bigliplus.model.action.Action;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ActionRepository extends JpaRepository<Action, Long> {
-}
